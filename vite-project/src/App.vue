73087c1f83ed7610137dc58e291bcfd2cb36cd02
PaulaSousa2014 <110301198+PaulaSousa2014@users.noreply.github.com>
2022-10-13 16:11:22 +0200

    Iteration2

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -2,6 +2,16 @@
 <template>
   
 <Navbar />
+<br>
+<div>
+
+2 + 2 = {{sumNumbers(2, 2)}} <br>
+2 + 2 = {{2+2}}
+</div>
+<div>
+  {{printString('This is a string printed by a function.')}} <br>
+  {{'This is a String printed directly.' }}
+</div>
 <Footer />
 
 </template> 
@@ -10,6 +20,20 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
+
+import { ref } from 'vue'
+const num1 = ref(Number)
+const num2 = ref(Number)
+const sumNumbers = (num1, num2) => {
+    return num1 + num2;
+}
+
+const name = ref(String)
+const printString = (name) => {
+  return name
+}
+
+
 
 </script>
 
